c884d2b8d7b5fc131e46314ce21a672bf1c04c9c
TamirKol <tamirk9596@gmail.com>
2021-03-05 16:16:26 +0200

    station and dbservices

---
+++ JestAppProj/JestAppProj/Models/Stations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JestAppProj.Models.DAL;
+
+namespace JestAppProj.Models
+{
+    public class Stations
+    {
+        int Id;
+        string StationName;
+    }
+}
